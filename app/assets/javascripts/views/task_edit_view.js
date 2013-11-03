85e411b351cbb2c099db94737ed1e380b32feffb
Daniel Quan <djquan@gmail.com>
2013-11-02 21:14:31 -0700

    Implement editing functionality as you go through the form, not when you press submit

--- app/assets/javascripts/views/task_edit_view.js
+++ app/assets/javascripts/views/task_edit_view.js
@@ -2,11 +2,20 @@ Breath.Views.TaskEdit = Backbone.View.extend({
   template: JST['tasks/edit'],
 
   initialize: function(){
-
+    this.listenTo(this.model, "add remove sync change", this.render);
   },
 
   events: {
+    'blur input': 'updateTask'
+  },
 
+  updateTask: function(event){
+    var target = $(event.currentTarget).attr('name');
+    var payload = $(event.currentTarget).val();
+    this.model.save(target, payload, {
+      success: function(obj){
+      }
+    })
   },
 
   render: function(){
--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -6,6 +6,19 @@ class Api::TasksController < ApplicationController
     render json: task
   end
 
+  def update
+    task = Task.find(params[:id])
+    if current_user == task.creator
+      if task.update_attributes(task_params)
+        render json: task
+      else
+        render task.errors.full_messages, status: :unprocessable_entity
+      end
+    else
+      render json: "You are not the creator"
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(:name, :description, :due, 
