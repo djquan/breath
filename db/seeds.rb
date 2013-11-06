3403b9b305402d15724483d885a38969294e8213
Daniel Quan <djquan@gmail.com>
2013-11-06 14:24:30 -0800

    Add some seed data and add a date stamp for the task index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
-User.create(name: "Dan Quan", email: "djquan@gmail.com", password: "123456")
-User.create(name: "John Smith", email: "johnsmith@example.com", password: "123456")
-User.create(name: "Jane Doe", email: "janedoe@example.com", password: "123456")
-User.create(name: "Boss Man", email: "boss_man@example.com", password: "123456")
-User.create(name: "Boss Woman", email: "boss_woman@example.com", password: "123456")
-User.create(name: "WorkerB", email: "workerB@example.com", password: "123456")
+dq = User.create(name: "Dan Quan", email: "djquan@gmail.com", password: "123456")
+js = User.create(name: "John Smith", email: "johnsmith@example.com", password: "123456")
+jd = User.create(name: "Jane Doe", email: "janedoe@example.com", password: "123456")
+bm = User.create(name: "Boss Man", email: "boss_man@example.com", password: "123456")
+bw = User.create(name: "Boss Woman", email: "boss_woman@example.com", password: "123456")
+wb = User.create(name: "WorkerB", email: "workerB@example.com", password: "123456")
+
+aa = Team.create(name: "App Academy")
+aa.users << [dq, js, jd]
