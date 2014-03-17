614f9710707fbfcc968591d41637fd3e4a1d295f
Daniel Quan <djquan@gmail.com>
2014-03-17 08:28:20 -0500

    Add hacky route to deal with RubyMotion not POSTing correctly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,19 @@ class SessionsController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find_by_credentials(params[:user][:name],
+                                     params[:user][:password])
+    if @user
+      login_user!(@user)
+      redirect_to app_url
+    else
+      flash.now[:errors] = ["Incorrect username or password"]
+      @user = User.new
+      render :new
+    end
+  end
+
   def destroy
     current_user.reset_session_token!
     session[:session_token] = nil
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Breath::Application.routes.draw do
   get "app", to: "root#root"
   resources :users
-  resource :session, only: [:create, :destroy]
+  resource :session, only: [:create, :destroy, :update]
 
   namespace "api", defaults: { format: :json } do 
     resources :projects, only: [:index, :show, :create, :destroy, :update]
