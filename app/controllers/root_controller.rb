90bd91c8891177d253a89f162c8929596b15e719
Daniel Quan <djquan@gmail.com>
2014-03-18 20:56:20 -0500

    Update controllers to handle some json edge cases

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,10 @@
 class RootController < ApplicationController
   def root
     if current_user
-      render :root
+      respond_to do |format|
+        format.json { render :json => { successful: true } } 
+        format.html { render :root } 
+      end
     else
       redirect_to root_url
     end
