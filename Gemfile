4a56c7e03efc7e7881c3f4ac9be56c72fcbf74cf
Daniel Quan <djquan@gmail.com>
2014-11-15 13:31:12 -0800

    Pumafy

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.1.5'
 
 gem 'rails', '4.1.7'
+gem 'puma'
 
 gem 'pg'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,8 @@ Breath::Application.configure do
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
+  config.middleware.delete Rack::Lock
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
